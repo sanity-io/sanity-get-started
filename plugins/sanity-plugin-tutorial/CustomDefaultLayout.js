acca000008252c174048a77e0e4c4b9219b98688
Rita <rita@sanity.io>
2022-03-23 14:22:36 +0100

    refactor(cli): add restriction to banner when not logged in

--- plugins/sanity-plugin-tutorial/CustomDefaultLayout.js
+++ plugins/sanity-plugin-tutorial/CustomDefaultLayout.js
@@ -2,10 +2,14 @@ import React from "react";
 import StudioRoot from "part:@sanity/default-layout/root";
 import HelloSanityTutorial from "./HelloSanityTutorial";
 
+import { useCurrentUser } from "@sanity/base/hooks";
+
 export default function CustomDefaultLayout() {
+  const currentUser = useCurrentUser();
+
   return (
     <>
-      <HelloSanityTutorial />
+      {currentUser.value && <HelloSanityTutorial />}
       <StudioRoot />
     </>
   );
